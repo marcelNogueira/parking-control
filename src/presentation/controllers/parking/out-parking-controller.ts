1b3e186787bb3b11b7ac72e8d59fa3afe1fc6d34
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2021-02-26 18:26:38 -0300

    fix: change getModel to calculate stay time on out parking

--- src/presentation/controllers/parking/out-parking-controller.ts
+++ src/presentation/controllers/parking/out-parking-controller.ts
@@ -1,12 +1,21 @@
 import { ParkingModel } from '@/domain/models/parking'
 import { OutParkingModel } from '@/domain/usecases/parking'
 import { GenericUpdateController } from '@/presentation/controllers/generic'
+import { HttpRequest } from '@/presentation/protocols';
 
 export class OutParkingController extends GenericUpdateController<
   ParkingModel,
   OutParkingModel
 > {
-  getModel() {
-    return { left: true };
+  getModel(httpRequest: HttpRequest) {
+    const inDate = httpRequest.createdAt;
+    const leftDate = new Date();
+    let seconds = (leftDate.valueOf() - inDate.valueOf())/1000;
+    const hours = Math.floor(seconds / (60*60));
+    seconds -= hours * 60 * 60;
+    const minutes = Math.floor(seconds / (60));
+    seconds -= minutes * 60;
+    const time = `${hours ? `${hours} hours ` : ''}${minutes ? `${minutes} minutes ` : ''}${seconds ? `${Math.floor(seconds)} seconds` : ''}`
+    return { left: true, time };
   }
 }
