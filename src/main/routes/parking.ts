cabd61e68fe267b56fdbdf5a1b02d623cd377a1c
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2021-02-26 18:26:55 -0300

    feat: out parking route

--- src/main/routes/parking.ts
+++ src/main/routes/parking.ts
@@ -1,18 +1,12 @@
 import { Router } from 'express'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { makeAddParkingController, makePayParkingController } from '@/main/factories/parking'
-// import { makeAdminNewPasswordEmailController } from '../factories/user/password/admin-new-password-email-factory'
-// import { makeGetAllUsersController } from '../factories/user/get/get-all-users-controller-factory'
-// import { makeGetUserController } from '../factories/user/get/get-user-controller-factory'
-// import { makeGetUsersByCompanyIdFactory } from '../factories/user/get/get-users-by-company-id-factory'
-// import { makeLoginController } from '../factories/user/login/login-controller-factory'
-// import { resetPasswordController } from '../factories/user/password/reset-password-factory'
-// import { sendRecoveryEmailController } from '../factories/user/password/send-recovery-password-email-factory'
-// import { makeSignupController } from '../factories/user/signup/signup-controller-factory'
-// import { updatePasswordController } from '../factories/user/password/update-password-controller-factory'
-// import { auth, adminAuth } from '../middlewares/auth'
+import { adaptMiddleware } from '../adapters/express-middleware-adapter';
+import { makeOutVerifierMiddleware } from '../factories/middlewares/out-verifier-middleware-factory';
+import { makeOutParkingController } from '../factories/parking/out-parking-factory';
 
 export default (router: Router): void => {
   router.post('/parking', adaptRoute(makeAddParkingController()));
   router.patch('/parking/:id/pay', adaptRoute(makePayParkingController()));
+  router.patch('/parking/:id/out', adaptMiddleware(makeOutVerifierMiddleware()), adaptRoute(makeOutParkingController()));
 }
