6cfac4fadceb3705d9c4dc70e9b4de7578b0fa41
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2021-02-26 15:32:50 -0300

    test: update add parking controller test to ensure that the parkingNumber is returned as controller response

--- src/presentation/controllers/parking/add-parking-controller.spec.ts
+++ src/presentation/controllers/parking/add-parking-controller.spec.ts
@@ -1,27 +1,48 @@
 import { MockParkingFactory } from '@/domain/factories/parking'
 import { ParkingModel } from '@/domain/models/parking'
-import { AddParkingModel } from '@/domain/usecases/parking'
+import { AddParking, AddParkingModel } from '@/domain/usecases/parking'
 import {
   GenericAddControllerSutModel,
   GenericAddControllerTestSuite,
   GenericAddInterfaceStub,
 } from '@/presentation/controllers/generic'
 import { ValidationStub } from '@/presentation/controllers/generic'
+import { MissingParamError, ServerError } from '@/presentation/errors'
+import { badRequest, serverError, success } from '@/presentation/helpers/http/http-helper'
+import { HttpRequest } from '@/presentation/protocols'
 import { AddParkingController } from './add-parking-controller'
 
-const tests = new GenericAddControllerTestSuite<
-  ParkingModel,
-  AddParkingModel
->()
+const throwError = (): never => {
+  throw new Error()
+}
 const mockFactory = new MockParkingFactory()
-const addStub = new GenericAddInterfaceStub(mockFactory)
+const fakeResponse = mockFactory.makeMockAddResponseModel();
+const makeFakeRequest = (): HttpRequest => ({
+  body: mockFactory.makeMockAddModel(),
+});
+
+class AddParkingInterfaceStub implements AddParking {
+  constructor(private readonly mockFactory: MockParkingFactory) {}
+  async add(): Promise<any> {
+    return await new Promise((resolve) => {
+      const parking = this.mockFactory.makeMockModel();
+      resolve(parking);
+    });
+  }
+}
+
+const addStub = new AddParkingInterfaceStub(mockFactory)
 const validationStub = new ValidationStub()
 const controller = new AddParkingController(addStub, validationStub)
 
-const makeAddParkingControllerSutModel = (): GenericAddControllerSutModel<
-  ParkingModel,
-  AddParkingModel
-> => {
+interface AddParkingControllerSutTypes {
+  controller: AddParkingController;
+  addStub: AddParkingInterfaceStub;
+  validationStub: ValidationStub;
+  mockFactory: MockParkingFactory;
+}
+
+const makeAddParkingControllerSutModel = (): AddParkingControllerSutTypes => {
   class AddParkingControllerSutModel
     implements
       GenericAddControllerSutModel<ParkingModel, AddParkingModel> {
@@ -33,7 +54,33 @@ const makeAddParkingControllerSutModel = (): GenericAddControllerSutModel<
   return new AddParkingControllerSutModel()
 }
 
-describe(
-  'Add Parking Controller',
-  tests.genericAddTestSuite(makeAddParkingControllerSutModel())
-)
+describe('Add Parking Controller', () => {
+  test('Should call Add Parking with correct values', async () => {
+    const { controller, addStub } = makeAddParkingControllerSutModel()
+    const addSpy = jest.spyOn(addStub, 'add')
+    await controller.handle(makeFakeRequest())
+    expect(addSpy).toHaveBeenCalledWith(mockFactory.makeMockAddModel())
+  })
+  
+  test('Should throw 500 if Add Parking throws', async () => {
+    const { controller, addStub } = makeAddParkingControllerSutModel()
+    jest.spyOn(addStub, 'add').mockImplementationOnce(throwError)
+    const response = await controller.handle(makeFakeRequest())
+    expect(response).toEqual(serverError(new ServerError(null)))
+  })
+  
+  test('Should return 400 if validation returns', async () => {
+    const { controller, validationStub } = makeAddParkingControllerSutModel()
+    if (validationStub) {
+      jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_param'))
+      const response = await controller.handle(makeFakeRequest())
+      expect(response).toEqual(badRequest(new MissingParamError('any_param')))
+    }
+  })
+  
+  test('Should return success if object is valid', async () => {
+    const { controller } = makeAddParkingControllerSutModel()
+    const adddPartner = await controller.handle(makeFakeRequest())
+    expect(adddPartner).toEqual(success(fakeResponse))
+  })
+})
