de8a85b667a3a61568113d89023ce90e8d597dd6
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2021-03-01 16:06:51 -0300

    feat: get history route

--- src/main/routes/parking.ts
+++ src/main/routes/parking.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
-import { makeAddParkingController, makePayParkingController } from '@/main/factories/parking'
+import { makeAddParkingController, makeGetHistoryParkingController, makePayParkingController, makeOutParkingController } from '@/main/factories/parking'
 import { adaptMiddleware } from '../adapters/express-middleware-adapter';
-import { makeOutVerifierMiddleware } from '../factories/middlewares/out-verifier-middleware-factory';
-import { makeOutParkingController } from '../factories/parking/out-parking-factory';
+import { makeOutVerifierMiddleware } from '../factories/middlewares';
 
 export default (router: Router): void => {
   router.post('/parking', adaptRoute(makeAddParkingController()));
   router.patch('/parking/:id/pay', adaptRoute(makePayParkingController()));
   router.patch('/parking/:id/out', adaptMiddleware(makeOutVerifierMiddleware()), adaptRoute(makeOutParkingController()));
+  router.get('/parking/:plate', adaptRoute(makeGetHistoryParkingController()));
 }
